92607c9777168481d85f0d810def04a0b56a7853
Zachy24 <Zachary.Howard@outlook.com>
2019-04-19 21:39:20 -0700

    Abstract Shaders to folder

--- OpenGLGame/OpenGLGame.cpp
+++ OpenGLGame/OpenGLGame.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 // GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
@@ -7,11 +5,87 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <chrono>
+#include <vector>
+#include <filesystem>
 
-using namespace std;
+static std::string readFile(const GLchar *filePath) {
+	std::string content;
+	std::ifstream fileStream(filePath, std::ios::in);
+
+	if (!fileStream.is_open()) {
+		std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+		return "";
+	}
+
+	std::string line = "";
+	while (!fileStream.eof()) {
+		std::getline(fileStream, line);
+		content.append(line + "\n");
+	}
+
+	fileStream.close();
+	return content;
+}
+
+static GLuint LoadShader(const char *vertex_path, const char *fragment_path) {
+	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Read shaders
+	std::string vertShaderStr = readFile(vertex_path);
+	std::string fragShaderStr = readFile(fragment_path);
+	const char* vertShaderSrc = vertShaderStr.c_str();
+	const char* fragShaderSrc = fragShaderStr.c_str();
+
+	GLint result = GL_FALSE;
+	int logLength;
+
+	// Compile vertex shader
+	std::cout << "Compiling vertex shader." << std::endl;
+	glShaderSource(vertShader, 1, &vertShaderSrc, NULL);
+	glCompileShader(vertShader);
+
+	// Check vertex shader
+	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+	std::vector<char> vertShaderError((logLength > 1) ? logLength : 1);
+	glGetShaderInfoLog(vertShader, logLength, NULL, &vertShaderError[0]);
+	std::cout << &vertShaderError[0] << std::endl;
+
+	// Compile fragment shader
+	std::cout << "Compiling fragment shader." << std::endl;
+	glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
+	glCompileShader(fragShader);
+
+	// Check fragment shader
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
+	glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
+	std::vector<char> fragShaderError((logLength > 1) ? logLength : 1);
+	glGetShaderInfoLog(fragShader, logLength, NULL, &fragShaderError[0]);
+	std::cout << &fragShaderError[0] << std::endl;
+
+	std::cout << "Linking program" << std::endl;
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertShader);
+	glAttachShader(program, fragShader);
+	glLinkProgram(program);
+
+	glGetProgramiv(program, GL_LINK_STATUS, &result);
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+	std::vector<char> programError((logLength > 1) ? logLength : 1);
+	glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
+	std::cout << &programError[0] << std::endl;
+
+	glDeleteShader(vertShader);
+	glDeleteShader(fragShader);
+
+	return program;
+}
+
 
 int main()
 {
@@ -31,11 +105,11 @@ int main()
 	Verticies. (X, Y, R, G, B)
 	*/
 	float vertices[] = {
-	0.0f, 0.5f, 1.0f, 0.0f, 0.0f,
-	0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-	-0.5f, -0.5f, 0.0f, 0.0f, 1.0f,	};
-
-
+		-0.5f, 0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+		0.5f, 0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f // Bottom-left
+	};
 	// Create a vbo
 	GLuint vbo;
 	glGenBuffers(1, &vbo); // Generate 1 buffer
@@ -50,49 +124,15 @@ int main()
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
 
-	std::string vertexShaderString =
-		"#version 150\n"
-		"in vec2 position;\n"
-		"in vec3 color;\n"
-		"out vec3 Color;\n"
-		"void main(){\n"
-		"Color = color;\n"
-		"gl_Position = vec4(position, 0.0, 1.0);}";
+	GLuint elements[] = {
+		0, 1, 2,
+		2, 3, 0
+	};	/*
+	Create a ebo. Page 33
+	*/	GLuint ebo;
+	glGenBuffers(1, &ebo);
 
-	const char *vertexShaderSource = vertexShaderString.c_str();
-
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	GLint vertexStatus;
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexStatus);
-	cout << "Vertex Shader Compile Status: " << vertexStatus << endl;
-
-	std::string fragmentShaderString =
-		"#version 150\n"
-		"in vec3 Color;\n"
-		"out vec4 outColor;\n"
-		"void main(){\n"
-		"outColor = vec4(Color, 1.0);}";
-
-	const char* fragmentShaderSource = fragmentShaderString.c_str();
-
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	GLint fragmentStatus;
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentStatus);
-	cout << "Fragment Shader Compile Status: " << fragmentStatus << endl;
-	
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-
-	glBindFragDataLocation(shaderProgram, 0, "outColor");
-
-	glLinkProgram(shaderProgram);
+	GLuint shaderProgram = LoadShader("Shaders\\Vertex.shader", "Shaders\\Fragment.shader");
 	glUseProgram(shaderProgram);
 
 	glBindVertexArray(vao); // Bind the vao to this program. Whenever glVertexAttribPointer is called, and this vao is bound, it will store that in this vao.
@@ -116,7 +156,7 @@ int main()
 	*/
 
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-	cout << "Position Input of vertex shader: " << posAttrib << endl;
+	std::cout << "Position Input of vertex shader: " << posAttrib << std::endl;
 
 	glEnableVertexAttribArray(posAttrib);
 	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0); 	
@@ -124,34 +164,15 @@ int main()
 	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
 	glEnableVertexAttribArray(colAttrib);
 	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 	while (!glfwWindowShouldClose(window)){
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 			glfwSetWindowShouldClose(window, GL_TRUE);
 		}
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 	}
 	glfwTerminate();
-}
-
-std::string readFile(const char* filePath) {
-	std::string content;
-	std::ifstream fileStream(filePath, std::ios::in);
-
-	if (!fileStream.is_open()) {
-		std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
-		return "";
-	}
-
-	std::string line = "";
-	while (!fileStream.eof()) {
-		std::getline(fileStream, line);
-		content.append(line + "\n");
-	}
-
-	fileStream.close();
-	return content;
 }
--- Shaders/Fragment.shader
+++
@@ -1,9 +0,0 @@
-#version 150
-
-uniform vec3 triangleColor;
-out vec4 outColor;
-
-void main()
-{
-	outColor = vec4(triangleColor, 1.0);
-}// The last argument is transparency.
